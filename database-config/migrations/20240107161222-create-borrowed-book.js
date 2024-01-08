dabe39b4c5cab83e68ac05173b66bf2e62d1d582
Youssef Gabr <youssef.gabr12@gmail.com>
2024-01-08 10:05:34 +0200

    wrong database names

--- database-config/migrations/20240107161222-create-borrowed-book.js
+++ database-config/migrations/20240107161222-create-borrowed-book.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('borrowedBooks', {
+    await queryInterface.createTable('borrowedBook', {
       id: {
         allowNull: false,
         autoIncrement: true,
